9d27b7eff2fbca6aefa0a850333fcd4c02a358e3
Anastasia Lyuleva <nesti-1998@mail.ru>
2025-01-08 15:54:02 +0300

    Changes in TestData

--- src/test/java/DemoqaTestsWithRandomData.java
+++ src/test/java/DemoqaTestsWithRandomData.java
@@ -1,65 +1,64 @@
 import TestData.TestData;
 import org.junit.jupiter.api.Test;
 
-import static TestData.TestData.*;
-
 
 public class DemoqaTestsWithRandomData extends TestBase {
 
     RegistrationPage registrationPage = new RegistrationPage();
-
+    TestData testData = new TestData();
 
 
     @Test
     void successfulSearchTest() {
         registrationPage.openPage()
                 .removeBanners()
-                .setFirstName(firstName)
-                .setLastName(lastName)
-                .setUserEmail(userEmail)
-                .setUserNumber(userPhone)
-                .setUserGenter(userGenter)
-                .setUserHobby(userHobby)
-                .userSelectPicture(userSelectPicture)
-                .setUserAddress(userAddress)
-                .selectUserState(userState)
-                .selectUserCity(userCity)
-                .setSubject(userSubjects)
-                .dateOfBirth(userDateOfBirth, userMonth, userYear)
+                .setFirstName(testData.firstName)
+                .setLastName(testData.lastName)
+                .setUserEmail(testData.userEmail)
+                .setUserNumber(testData.userPhone)
+                .setUserGenter(testData.userGenter)
+                .setUserHobby(testData.userHobby)
+                .userSelectPicture(testData.userSelectPicture)
+                .setUserAddress(testData.userAddress)
+                .selectUserState(testData.userState)
+                .selectUserCity(testData.userCity)
+                .setSubject(testData.userSubjects)
+                .dateOfBirth(testData.userDateOfBirth, testData.userMonth, testData.userYear)
                 .submitButton()
-                .checkResult("Student Name", firstName + " "+lastName)
-                .checkResult("Student Email", userEmail)
-                .checkResult("Gender", userGenter)
-                .checkResult("Mobile", userPhone)
-                .checkResult("Date of Birth", userDateOfBirth + " "+ userMonth + ","+userYear)
-                .checkResult("Subjects", userSubjects)
-                .checkResult("Hobbies", userHobby)
-                .checkResult("Picture", userSelectPicture)
-                .checkResult("Address", userAddress)
-                .checkResult("State and City", userState + " "+ userCity);
+                .checkResult("Student Name", testData.firstName + " " + testData.lastName)
+                .checkResult("Student Email", testData.userEmail)
+                .checkResult("Gender", testData.userGenter)
+                .checkResult("Mobile", testData.userPhone)
+                .checkResult("Date of Birth", testData.userDateOfBirth + " " + testData.userMonth + "," + testData.userYear)
+                .checkResult("Subjects", testData.userSubjects)
+                .checkResult("Hobbies", testData.userHobby)
+                .checkResult("Picture", testData.userSelectPicture)
+                .checkResult("Address", testData.userAddress)
+                .checkResult("State and City", testData.userState + " " + testData.userCity);
 
 
     }
+
     @Test
     void minimalSuccessfulSearchTest() {
         registrationPage.openPage()
-                .setFirstName(firstName)
-                .setLastName(lastName)
-                .setUserGenter(userGenter)
-                .setUserNumber(userPhone)
+                .setFirstName(testData.firstName)
+                .setLastName(testData.lastName)
+                .setUserGenter(testData.userGenter)
+                .setUserNumber(testData.userPhone)
                 .submitButton()
-                .checkResult("Student Name",  firstName + " "+lastName)
-                .checkResult("Gender", userGenter)
-                .checkResult("Mobile", userPhone);
+                .checkResult("Student Name", testData.firstName + " " + testData.lastName)
+                .checkResult("Gender", testData.userGenter)
+                .checkResult("Mobile", testData.userPhone);
 
     }
 
     @Test
     void testRegistrationWithInvalidInput() {
         registrationPage.openPage()
-                .setFirstName(firstName)
-                .setLastName(lastName)
-                .setUserGenter(userGenter)
+                .setFirstName(testData.firstName)
+                .setLastName(testData.lastName)
+                .setUserGenter(testData.userGenter)
                 .submitButton()
                 .colorBorderInputUsernumber("border-color:#dc3545");
 
--- src/test/java/TestData/TestData.java
+++ src/test/java/TestData/TestData.java
@@ -2,40 +2,51 @@ package TestData;
 
 import com.github.javafaker.Faker;
 
-import java.util.Date;
 import java.util.Locale;
 
-
-
-
-
-
 public class TestData {
-    static Faker faker = new Faker(new Locale("en-GB"));
-    public static  String firstName = faker.name().firstName(),
-            lastName= faker.name().lastName(),
-            userEmail= faker.internet().emailAddress(),
-            userAddress=faker.address().cityName(),
-             userPhone= faker.phoneNumber().subscriberNumber(10),
-             userGenter= faker.options().option("Male", "Female", "Other"),
-             userHobby= faker.options().option("Sports", "Reading", "Music"),
-             userSelectPicture = faker.options().option( "Енот.jpg", "test1.PNG"),
-             userSubjects = faker.options().option( "Maths", "English"),
-             userState = faker.options().option( "NCR", "Haryana"),
-             userMonth = faker.options().option("January", "February", "March", "April", "May", "June", "July", "August", "September", "October", "November", "December"),
-             userDateOfBirth = String.format(String.valueOf(faker.number().numberBetween(1, 28))),
-             userYear = String.format(String.valueOf(faker.number().numberBetween(1998, 2024)));
+    private Faker faker;
 
-     public static String userCity;
+    public String firstName,
+            lastName,
+            userEmail,
+            userAddress,
+            userPhone,
+            userGenter,
+            userHobby,
+            userSelectPicture,
+            userSubjects,
+            userState,
+            userMonth,
+            userDateOfBirth,
+            userYear,
+            userCity;
 
-      static { // Changed to static block for initialization
-             // Fixed equality check from single '=' to '=='  if (userState = "NCR" )
-             if (userState.equals("NCR")) {
-                 userCity = faker.options().option("Delhi", "Gurgaon", "Noida");
-             }
-             else
-             {
-                 userCity =faker.options().option( "Karnal", "Panipat");
-             }
+    public TestData() {
+        faker = new Faker(new Locale("en-GB"));
+        firstName = faker.name().firstName();
+        lastName = faker.name().lastName();
+        userEmail = faker.internet().emailAddress();
+        userAddress = faker.address().cityName();
+        userPhone = faker.phoneNumber().subscriberNumber(10);
+        userGenter = faker.options().option("Male", "Female", "Other");
+        userHobby = faker.options().option("Sports", "Reading", "Music");
+        userSelectPicture = faker.options().option("Енот.jpg", "test1.PNG");
+        userSubjects = faker.options().option("Maths", "English");
+        userState = faker.options().option("NCR", "Haryana");
+        userMonth = faker.options().option("January", "February", "March", "April", "May", "June", "July", "August", "September", "October", "November", "December");
+        userDateOfBirth = String.format("%d", faker.number().numberBetween(1, 29)); // Use 1 to 28
+        userYear = String.format("%d", faker.number().numberBetween(1998, 2024));
 
-    }}
+        setUserCity();
+    }
+
+
+    private void setUserCity() {
+        if (userState.equals("NCR")) {
+            userCity = faker.options().option("Delhi", "Gurgaon", "Noida");
+        } else {
+            userCity = faker.options().option("Karnal", "Panipat");
+        }
+    }
+}
