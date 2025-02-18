ac400dbe01dfac6783131cbd8ca6f25b78aaa832
Defoltini <nesti-1998@mail.ru>
2025-02-18 23:42:15 +0300

    added properties

--- src/test/java/TestBase.java
+++ src/test/java/TestBase.java
@@ -12,12 +12,12 @@ public class TestBase {
     @BeforeAll
     static void beforeAll() {
         Configuration.pageLoadStrategy = "eager";
-        Configuration.browserSize = "1920x1080";
+        //Configuration.browserSize = "1920x1080";
         Configuration.baseUrl = "https://demoqa.com";
         //Configuration.browser = "chrome";
         Configuration.timeout = 10000;
         //Configuration.holdBrowserOpen = true;
-        Configuration.remote = "https://user1:1234@selenoid.autotests.cloud/wd/hub";
+        Configuration.remote = "https://user1:1234@"+selenoidHost+"wd/hub";
         DesiredCapabilities capabilities = new DesiredCapabilities();
         capabilities.setCapability("selenoid:options", Map.<String, Object>of(
                 "enableVNC", true,
